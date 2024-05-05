3a4825d8f394e414cf75605a912de5cc87e93e2b
Mike Salvatore <mike.s.salvatore@gmail.com>
2024-05-05 13:55:07 -0400

    Add commas to stats requests for easy reading

--- gafm/stats.py
+++ gafm/stats.py
@@ -3,6 +3,8 @@
 import tabulate
 from redis import Redis
 
+THRESHOLD = 10
+
 r = Redis(host="localhost", port=6379, decode_responses=True)
 
 request_count = {k: int(r.get(k)) for k in r.keys()}  # type: ignore [arg-type]
@@ -10,15 +12,23 @@ request_count = {k: int(r.get(k)) for k in r.keys()}  # type: ignore [arg-type]
 all_keys = list(request_count.keys())
 all_keys.sort(key=request_count.get, reverse=True)  # type: ignore [arg-type]
 
-table: list[tuple[str, str, int]] = []
+data: list[tuple[str, str, int]] = []
 for k, _v in request_count.items():
+    if "-redirect" in k:
+        continue
+
     _, __, ip, date = k.split(":")
-    table.append((date, ip, request_count[k]))
+    if request_count[k] <= THRESHOLD:
+        continue
 
-table.sort(key=lambda x: (x[2], x[0], x[1]), reverse=True)
+    data.append((date, ip, request_count[k]))
+
+data.sort(key=lambda x: (x[0], x[2], x[1]), reverse=True)
+
+table = [(x[0], x[1], f"{x[2]:,}") for x in data]
 
 print()
-print("\033[33mNOTE: ONLY SHOWING ROWS WHERE REQUESTS > 3\033[0m")
+print(f"\033[33mNOTE: ONLY SHOWING ROWS WHERE REQUESTS > {THRESHOLD}\033[0m")
 print()
-print(tabulate.tabulate([row for row in table if row[2] > 3], headers=["Date", "IP", "# requests"]))
+print(tabulate.tabulate(table, headers=["Date", "IP", "# requests"]))
 print()
